63ffaf542d147bf95045cb5628a8e6f11b8aa8cc
Mostafa1201 <mostafa1201@hotmail.com>
2023-05-23 15:59:19 +0300

    - Move request logic to a hook and use it inside of signup component

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import axios from 'axios';
+import UseRequest from "../../hooks/use-request";
 
 const SignUp = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
-
+    const { doRequest, errors} = UseRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        }
+    });
     const onSubmit = async event => {
         event.preventDefault();
-        
-        try {
-            const response = await axios.post('/api/users/signup',{
-                email,password
-            });
-            
-            console.log(response.data);
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
+        doRequest();
     };
 
     return <form onSubmit={onSubmit}>
@@ -41,14 +38,7 @@ const SignUp = () => {
                 className="form-control" 
             />
         </div>
-        {errors.length > 0 && (
-            <div className="alert alert-danger">
-                <h4>Oooops....</h4>
-                <ul className="my-0">
-                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
-                </ul>
-            </div>
-        )}
+        {errors}
         <button className="btn btn-primary">Sign Up</button>
     </form>;
 };
