a5be897f16a9174940336dabcb8b70b405ed0d17
Mostafa1201 <mostafa1201@hotmail.com>
2023-05-23 20:05:02 +0300

    - use the build client function instead of using axios directly

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,16 @@
-const LandingPage = () => {
-    return <h1>Landing Page</h1>;
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
-   
+
+// This is executed during the server side rendering
+// It also gets executed on the client whenever we navigate from one route to another
+// INSIDE the application
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
 export default LandingPage;
