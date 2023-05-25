685d12ef85f3ac32845b6806ac36b403b31acd2a
Mostafa1201 <mostafa1201@hotmail.com>
2023-05-25 23:11:54 +0300

    - Initialize tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler , NotFoundError} from '@mt_tickets/common';
+
+const app = express();
+// to trust proxy for ingress nginx
+app.set('trust proxy',true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+app.all('*', async () => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler);
+
+export { app };
