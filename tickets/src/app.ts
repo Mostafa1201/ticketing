07186563cc3eb94881b8a6ad6b0823df0c7af1f0
Mostafa1201 <mostafa1201@hotmail.com>
2023-05-28 00:49:47 +0300

    - Add currentuser middleware to app.ts

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler , NotFoundError} from '@mt_tickets/common';
+import { currentUser, errorHandler , NotFoundError} from '@mt_tickets/common';
 import { createTicketRouter } from './routes/new';
 
 const app = express();
@@ -16,6 +16,7 @@ app.use(
     })
 );
 
+app.use(currentUser);
 app.use(createTicketRouter);
 
 app.all('*', async () => {
