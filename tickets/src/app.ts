b5354e00b69e17470bcee48d26eb3f978f1c90f1
Mostafa1201 <mostafa1201@hotmail.com>
2023-05-28 01:27:17 +0300

    - add show ticket route and test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { currentUser, errorHandler , NotFoundError} from '@mt_tickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 // to trust proxy for ingress nginx
@@ -18,6 +19,7 @@ app.use(
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError()
