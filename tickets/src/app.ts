89ed8d910ada533780f87825334453ce4ce6b318
Mostafa1201 <mostafa1201@hotmail.com>
2023-05-28 02:20:48 +0300

    - Add update ticket route and test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import { currentUser, errorHandler , NotFoundError} from '@mt_tickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 // to trust proxy for ingress nginx
@@ -22,6 +23,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError()
