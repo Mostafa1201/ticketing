78dc3a8e468a2b1adfc112ae5ae7d346f33d33ef
Mostafa1201 <mostafa1201@hotmail.com>
2023-06-26 18:58:21 +0300

    # feat - publish ticket updates to sync the version number with order service

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCancelledEvent, OrderCreatedEvent, OrderStatus, Subjects
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -18,6 +19,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
         ticket.set({ orderId: data.id })
         // save the ticket
         await ticket.save();
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            orderId: ticket.orderId,
+            version: ticket.version,
+        });
         // ack the message
         msg.ack();
     }
