b568bf2ead9e7c7827826a07ce8bfe7f1fa73762
Mostafa1201 <mostafa1201@hotmail.com>
2023-07-09 02:40:12 +0300

    # issue - Fix an issue where current user api called twice

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from "../api/build-client";
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
@@ -7,10 +5,8 @@ const LandingPage = ({ currentUser }) => {
 // This is executed during the server side rendering
 // It also gets executed on the client whenever we navigate from one route to another
 // INSIDE the application
-LandingPage.getInitialProps = async context => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
