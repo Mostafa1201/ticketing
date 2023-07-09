d5dd12238c7d94807907f078236f04a1c6586b9b
Mostafa1201 <mostafa1201@hotmail.com>
2023-07-09 16:55:39 +0300

    # feat - create ticket list in landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,38 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
+const LandingPage = ({ currentUser , tickets }) => {
+
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 // This is executed during the server side rendering
 // It also gets executed on the client whenever we navigate from one route to another
 // INSIDE the application
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 };
 
 export default LandingPage;
